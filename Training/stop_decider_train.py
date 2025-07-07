2d244159ece8b1062a61eb3b0af53003a4afd065
Jaewan Park <jaejae1112@snu.ac.kr>
2025-07-07 15:03:53 +0900

    Fix nan errors for soft_diff

--- Training/stop_decider_train.py
+++ Training/stop_decider_train.py
@@ -68,6 +68,7 @@ def compute_loss_func(target_type="abs"):
             targets = labels[:, 0]
         elif target_type == "soft_diff":
             targets = labels[:, 0] / (labels[:, 0] + labels[:, 1])
+            targets = torch.nan_to_num(targets, nan=0.0)
         elif target_type == "hard_diff":
             targets = (labels[:, 0] > labels[:, 1]).float()
         else:
