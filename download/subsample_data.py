61ffa78ee05cb76e333e75beeea032f3296a3959
Jaewan Park <jaejae1112@snu.ac.kr>
2025-08-19 17:51:30 +0900

    Balance hops in data subsampling

--- download/subsample_data.py
+++ download/subsample_data.py
@@ -1,7 +1,8 @@
 import argparse
 import json
+import math
 import random as rd
-from config import DATASET_PATHS
+from config import DATASET_PATHS, DATASET_FIELDS
 
 
 def parse_args():
@@ -24,9 +25,34 @@ if __name__ == "__main__":
         else:
             data = json.load(f)
 
+    hop_counts = {}
+    for item in data:
+        num_hops = len(item[DATASET_FIELDS[args.dataset]["supporting_facts"]])
+        hop_counts[num_hops] = hop_counts.get(num_hops, 0) + 1
+
     rd.shuffle(data)
-    eval_data = data[:args.num_eval]
-    test_data = data[args.num_eval:args.num_eval + args.num_test]
+
+    eval_data = []
+    test_data = []
+
+    for num_hops, count in hop_counts.items():
+        eval_count = math.floor(count / len(data) * args.num_eval)
+        test_count = math.floor(count / len(data) * args.num_test)
+
+        hop_data = [item for item in data if len(item[DATASET_FIELDS[args.dataset]["supporting_facts"]]) == num_hops]
+        eval_data.extend(hop_data[:eval_count])
+        test_data.extend(hop_data[eval_count:eval_count + test_count])
+
+    remaining_data = [item for item in data if item not in eval_data and item not in test_data]
+    rd.shuffle(remaining_data)
+
+    missing_eval = args.num_eval - len(eval_data)
+    missing_test = args.num_test - len(test_data)
+    eval_data.extend(remaining_data[:missing_eval])
+    test_data.extend(remaining_data[missing_eval:missing_eval + missing_test])
+
+    rd.shuffle(eval_data)
+    rd.shuffle(test_data)
 
     with open(dataset_paths["eval_subsampled"], "w", encoding="utf-8") as f:
         if args.dataset == "musique":
